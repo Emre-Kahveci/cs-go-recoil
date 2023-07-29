16b95c1f9b4fbabdbcc4caeb45eaf8cc5874bc42
Emre-Kahveci <aominemre@gmail.com>
2023-07-29 18:41:59 +0300

    getting offsets at runtime

--- cs-go-recoil.py
+++ cs-go-recoil.py
@@ -1,15 +1,18 @@
 import pymem
 from time import sleep
+import requests
 
-offsets = {
-    "localPlayer": 0xDEA98C,
-    "clientState": 0x59F19C,
-    "clientState_ViewAngles": 0x4D90,
-    'aimPunchAngle': 0x303C,
-    'shotsFired': 0x103E0
-}
+def get_offset(url = "https://raw.githubusercontent.com/frk1/hazedumper/master/csgo.json"):
+    try:
+        response = requests.get(url)
+        response.raise_for_status()  # Check if the request was successful
+        json_data = response.json()
+        return json_data
+    except requests.exceptions.RequestException as e:
+        print(f"Error occurred while fetching the JSON file: {e}")
+        return None
 
-def recoil() -> None:
+def recoil(offsets = get_offset()) -> None:
     pm = pymem.Pymem('csgo.exe') # find csgo.exe
     
     client = pymem.process.module_from_name(pm.process_handle, "client.dll").lpBaseOfDll # access client.dll
@@ -20,17 +23,17 @@ def recoil() -> None:
     while True:
         sleep(.01)
 
-        localPlayer = pm.read_uint(client + offsets["localPlayer"])
-        shotsFired = pm.read_int(localPlayer + offsets["shotsFired"])
+        localPlayer = pm.read_uint(client + offsets["signatures"]["dwLocalPlayer"])
+        shotsFired = pm.read_int(localPlayer + offsets["netvars"]["m_iShotsFired"])
 
         if shotsFired: 
 
-            clientState = pm.read_uint(engine + offsets["clientState"])
-            viewAnglesX = pm.read_float(clientState + offsets["clientState_ViewAngles"])
-            viewAnglesY = pm.read_float(clientState + offsets["clientState_ViewAngles"] + 4)
+            clientState = pm.read_uint(engine + offsets["signatures"]["dwClientState"])
+            viewAnglesX = pm.read_float(clientState + offsets["signatures"]["dwClientState_ViewAngles"])
+            viewAnglesY = pm.read_float(clientState + offsets["signatures"]["dwClientState_ViewAngles"] + 4)
 
-            aimPunchX = pm.read_float(localPlayer + offsets["aimPunchAngle"])
-            aimPunchY = pm.read_float(localPlayer + offsets["aimPunchAngle"] + 4)
+            aimPunchX = pm.read_float(localPlayer + offsets["netvars"]["m_aimPunchAngle"])
+            aimPunchY = pm.read_float(localPlayer + offsets["netvars"]["m_aimPunchAngle"] + 4)
 
             newAngles = (
                 viewAnglesX + oldPunch[0] - aimPunchX * 2.0,
@@ -61,8 +64,8 @@ def recoil() -> None:
                     newAngles[1] + 360.0
                     )
 
-            pm.write_float(clientState + offsets["clientState_ViewAngles"], newAngles[0])
-            pm.write_float(clientState + offsets["clientState_ViewAngles"] + 4, newAngles[1])
+            pm.write_float(clientState + offsets["signatures"]["dwClientState_ViewAngles"], newAngles[0])
+            pm.write_float(clientState + offsets["signatures"]["dwClientState_ViewAngles"] + 4, newAngles[1])
             
             oldPunch = (aimPunchX * 2.0, aimPunchY * 2.0)
         else:
